896bc0812b64f36fec76f11e3686d9d87548666e
alpo6259 <alpo6259@gmail.com>
2022-02-25 14:05:44 +0200

    Add js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 `use strict`;
+
+function printNumbers(from, to, interval) {
+  let startValue = from;
+
+  let getTimer = setInterval(function () {
+    console.log(startValue);
+    if (startValue >= to) {
+      clearInterval(getTimer);
+    }
+    startValue += interval;
+  }, 500);
+}
+
+printNumbers(1, 10, 2); //
